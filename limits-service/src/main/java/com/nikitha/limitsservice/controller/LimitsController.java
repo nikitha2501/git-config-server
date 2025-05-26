9720de9b97fd51659c7e51231a5cec9fe9b3caf3
Nikitha <vnikitha99@gmail.com>
2025-05-26 18:55:54 +0530

    git_global_config repo

---
+++ limits-service/src/main/java/com/nikitha/limitsservice/controller/LimitsController.java
@@ -0,0 +1,21 @@
+package com.nikitha.limitsservice.controller;
+
+import com.nikitha.limitsservice.configuration.Configuration;
+import com.nikitha.limitsservice.model.Limits;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LimitsController {
+
+    @Autowired
+    private Configuration configuration;
+
+    @GetMapping("/limits")
+    public Limits retrieveLimits(){
+
+        return new Limits(configuration.getMinimum(),configuration.getMaximum());
+
+    }
+}
